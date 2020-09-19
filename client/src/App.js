a9438988b32556cb8adc29c888326b3f049aede1
Ieva Kaziukonyte <kaziukonyte.ieva@gmail.com>
2020-09-19 18:14:25 +0200

    put sign in state in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GoogleAuth from "./components/GoogleAuth";
 
+const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
 const App = () => {
-  return (
-    <div>
-      <GoogleAuth />
-    </div>
-  );
+  const [auth, setGoogleAuth] = useState(null);
+  const [isSignedIn, setIsSignedIn] = useState(null);
+
+  useEffect(() => {
+    window.gapi.load("client:auth2", () => {
+      // initializing the library
+      window.gapi.client
+        .init({
+          clientId: CLIENT_ID,
+          scope: "email",
+        })
+        .then(() => {
+          // provides methods to allow the user to sign in with a Google account
+          const GoogleAuth = window.gapi.auth2.getAuthInstance();
+          setGoogleAuth(GoogleAuth);
+          setIsSignedIn(GoogleAuth.isSignedIn.get());
+        });
+    });
+  }, []);
+
+  const renderTest = (isSignedIn) => {
+    return isSignedIn ? "you are signed in" : <GoogleAuth auth={auth} />;
+  };
+
+  return <div>{renderTest(isSignedIn)}</div>;
 };
 
 export default App;
--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,26 +1,7 @@
-import React, { useEffect, useState } from "react";
-const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+import React from "react";
 
-const GoogleAuth = () => {
-  const [auth, setGoogleAuth] = useState(null);
-  useEffect(() => {
-    window.gapi.load("client:auth2", () => {
-      // initializing the library
-      window.gapi.client
-        .init({
-          clientId: CLIENT_ID,
-          scope: "email",
-        })
-        .then(() => {
-          // provides methods to allow the user to sign in with a Google account
-          const GoogleAuth = window.gapi.auth2.getAuthInstance();
-          setGoogleAuth(GoogleAuth);
-        });
-    });
-  }, []);
-
-  console.log("this should be initialized", auth);
-  return <div>Button</div>;
+const GoogleAuth = ({ auth }) => {
+  return <button onClick={() => auth.signIn()}>Sign In with Google</button>;
 };
 
 export default GoogleAuth;
