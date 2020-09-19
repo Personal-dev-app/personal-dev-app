6e297b009157db23daca5a3348cb03f80ab8aaf4
Marco Muser <marco.muser@posteo.de>
2020-09-19 09:18:25 +0200

    add mongoose & configure MongoDB-Atlas db access

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+mongoose
+  .set("useUnifiedTopology", true)
+  .set("useFindAndModify", false)
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/personal-dev-app", {
+    useNewUrlParser: true,
+  })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo:", err);
+  });
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
